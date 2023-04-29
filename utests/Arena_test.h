0a623b68d206a0dc530b92597b83042be981fec2
Liza Malinova <malinova.liza04@mail.ru>
2023-04-29 21:07:12 +0300

    Добавлен тест на оверран

--- utests/Arena_test.h
+++ utests/Arena_test.h
@@ -19,3 +19,18 @@ struct ArenaTest : public testing::Test {
 		delete arena;
 	}
 };
+
+TEST(ArenaTest, heck_Over_Run) {
+
+	int x[] = { 300, 500, 3456, 123456 };
+	int y[] = { 782, 2345, -2345, 344 };
+	Arena* arena = new Arena(6, 7, prey, predator);
+
+	for (int i = 0; i < std::size(x); i++) {
+		for (int j = 0; j < std::size(y); j++) {
+			Point2D point(x[i], y[j]);
+			predator->moveTo(x[i], y[i]);
+			EXPECT_EQ(arena->checkOverRun(), true);
+		}
+	}
+}
